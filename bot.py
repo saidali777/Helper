345fdc035042094a22cb046ca33814c69b2a4aed
saidali777 <148846780+saidali777@users.noreply.github.com>
2025-05-22 19:46:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # worker: python script.py --> Procfile
 # heroku ps:scale worker=1 --> CLI
 from telegram import InlineQueryResultArticle, InputTextMessageContent, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.constants import ParseMode
+from telegram import ParseMode  # ✅ correct for v13.15
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters, ConversationHandler
 from credentials import bot_token
 from helper_functions import *
